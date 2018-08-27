fb51276b05084781eb14076e8e5cc884f315732f
mddemarie <mddemarie@gmail.com>
2018-08-27 12:12:27 +0200

    requesting 1 temperature for testing purpose

--- temperature/views.py
+++ temperature/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+import datetime
+
+from client import TemperatureClient
+
+
+def get_temperatures():
+    temperature_client = TemperatureClient()
+    date = '{}Z'.format(datetime.datetime.now().replace(microsecond=0).isoformat())
+    url = '?'
+    params = {
+        'at': date
+    }
+    resp = temperature_client.request(url, 'get', params)
+    return resp.json()
+
+print(get_temperatures())
 
 class TempertureList():
     pass
