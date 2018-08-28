247cfbf10c1091ff2fcfa5c49529f86d79d9dd8e
mddemarie <mddemarie@gmail.com>
2018-08-28 13:53:47 +0200

    build a few methods for getting all temperatures of given period & adjusted the views

--- temperature/serializers.py
+++ temperature/serializers.py
@@ -1,20 +1,43 @@
 """
 Just a fake serializer. No database needed.
 """
-import datetime
+from datetime import datetime, timedelta
+import dateutil.parser
 
 from .client import TemperatureClient
 
 class TemperatureSerializer(object):
 
-    def get_temperature(self):
+    def get_temperature_from_client(self, date):
         temperature_client = TemperatureClient()
-        date = '{}Z'.format(datetime.datetime.now().replace(microsecond=0).isoformat())
         params = {'at': date}
         resp = temperature_client.request('get', params)
         return resp.json()
 
-    @property
-    def data(self, context={}): # I will use the context later on to try sth.
-        self.get_temperature()
-        
+    def get_datetime_from_ISO8601(self, string):
+        dt_result = dateutil.parser.parse(string)
+        return dt_result
+
+    def get_delta(self, start, end):
+        first_date = self.get_datetime_from_ISO8601(start)
+        second_date = self.get_datetime_from_ISO8601(end)
+        delta = second_date - first_date
+        diff_in_days = delta.days
+        return diff_in_days
+
+    def data(self, start_date, end_date):
+        delta = self.get_delta(start_date, end_date)
+        result_data = []
+
+        for day in range(delta + 1):
+            one_snippet = self.get_temperature_from_client(start_date)
+            result_data.append(one_snippet)
+            start_date = self.get_datetime_from_ISO8601(start_date) + timedelta(days=1)
+            start_date = start_date.isoformat()
+
+        return result_data
+
+
+# For testing purposes:
+# serializer = TemperatureSerializer()
+# print(serializer.data("2018-08-01T00:00:00Z", "2018-08-07T00:00:00Z"))
--- temperature/views.py
+++ temperature/views.py
@@ -10,10 +10,10 @@ class TemperatureList(APIView):
     """
     List all temperatures between start_date and end_date.
     """
-    def get(self, request, format=None):
+    def get(self, request, start_date, end_date):
         serializer = TemperatureSerializer()
         context = {
             'start': start_date,
             'end': end_date
             }
-        return Response().render(data=serializer.data, renderer_context=context)
+        return Response().render(data=serializer.data(start_date, end_date), renderer_context=context)
