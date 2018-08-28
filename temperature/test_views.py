525e0c5cf7de557c04fc9bbc64e8f3dc4943f20c
mddemarie <mddemarie@gmail.com>
2018-08-28 20:41:28 +0200

    wrote first integration test

--- temperature/test_views.py
+++ temperature/test_views.py
@@ -1,5 +1,67 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-class TemperaturesTest(APITestCase):
-    pass
+from .serializers import TemperatureSerializer
+
+
+class TemperatureTest(APITestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.url = '/temperatures'
+        cls.start = '2018-08-12T00:00:00Z'
+        cls.end = '2018-08-14T00:00:00Z'
+
+        # create a temperature data for GET method
+        cls.temperatures = [
+            {'temp': 12.037116255932265, 'date': '2018-08-01T00:00:00Z'},
+            {'temp': 10.748827693846698, 'date': '2018-08-02T00:00:00Z'},
+            {'temp': 9.55776925245165, 'date': '2018-08-03T00:00:00Z'},
+            {'temp': 12.293283312397936, 'date': '2018-08-04T00:00:00Z'},
+            {'temp': 9.061370411273243, 'date': '2018-08-05T00:00:00Z'},
+            {'temp': 12.071404446393888, 'date': '2018-08-06T00:00:00Z'},
+            {'temp': 10.63004921027248, 'date': '2018-08-07T00:00:00Z'}
+            ]
+
+        # create a list of temperature snippets for GET method with fake serializer
+        cls.serializer = TemperatureSerializer()
+        cls.temperature_list = cls.serializer.data(cls.start, cls.end)
+
+        cls.temperature_url = '{}?start={}&end={}'.format(cls.url, cls.start, cls.end) 
+
+    def test_list_temperatures(self):
+        """
+        See the list of temperatures.
+        """
+        response = self.client.get(self.temperature_url)
+        print('##########', response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+        self.assertEqual(len(response.data), 1)
+
+    # def test_list_temperatures_fails_without_start_date(self):
+    #     """
+    #     Start date cannot be empty.
+    #     """
+    #     response = self.client.get(self.url, { # fix this
+    #         k: v for (k, v) in self.order_post.items()
+    #         if k is not 'size'
+    #     })
+    #     self.assertEqual(
+    #         response.status_code, status.HTTP_400_BAD_REQUEST, response.data
+    #     )
+    #     self.assertEqual(response.data['start'], ['This field is required.'])
+
+    # def test_list_temperatures_fails_without_end_date(self):
+    #     """
+    #     Start date field cannot be empty.
+    #     """
+    #     response = self.client.get(self.url, { # fix this
+    #         k: v for (k, v) in self.order_post.items()
+    #         if k is not 'customer_name'
+    #     })
+    #     self.assertEqual(
+    #         response.status_code, status.HTTP_400_BAD_REQUEST, response.data
+    #     )
+    #     self.assertEqual(
+    #         response.data['end'], ['This field is required.']
+    #     )
