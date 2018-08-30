116ce4f76922dc7ba695ba48996d16938e951829
mddemarie <mddemarie@gmail.com>
2018-08-30 15:48:42 +0200

    fixed the test recursion error

--- temperature/test_views.py
+++ temperature/test_views.py
@@ -3,12 +3,12 @@ from rest_framework.test import APITestCase
 from rest_framework.test import APIClient
 from urllib.parse import urlencode
 
-from .data import TemperatureSerializer
+from temperature.data import TemperatureSerializer
 
 
 class TemperatureTest(APITestCase):
     @classmethod
-    def setUpTestData(cls):
+    def setUpClass(cls):
         super().setUpClass()
         cls.url = '/temperatures'
         cls.start = '2018-08-12T00:00:00Z'
@@ -16,18 +16,13 @@ class TemperatureTest(APITestCase):
 
         # create a temperature data for GET method
         cls.temperatures = [
-            {'temp': 12.037116255932265, 'date': '2018-08-01T00:00:00Z'},
-            {'temp': 10.748827693846698, 'date': '2018-08-02T00:00:00Z'},
-            {'temp': 9.55776925245165, 'date': '2018-08-03T00:00:00Z'},
-            {'temp': 12.293283312397936, 'date': '2018-08-04T00:00:00Z'},
-            {'temp': 9.061370411273243, 'date': '2018-08-05T00:00:00Z'},
-            {'temp': 12.071404446393888, 'date': '2018-08-06T00:00:00Z'},
-            {'temp': 10.63004921027248, 'date': '2018-08-07T00:00:00Z'}
-            ]
+            {'temp': 12.037116255932265, 'date': '2018-08-12T00:00:00Z'},
+            {'temp': 10.748827693846698, 'date': '2018-08-13T00:00:00Z'},
+            {'temp': 9.55776925245165, 'date': '2018-08-14T00:00:00Z'}]
 
         # create a list of temperature snippets for GET method with fake serializer
         cls.serializer = TemperatureSerializer()
-        cls.temperature_list = cls.serializer.data(cls.start, cls.end)
+        cls.temperature_list = cls.serializer.all_data(cls.start, cls.end)
 
         cls.params = urlencode({'start': cls.start, 'end': cls.end})
         cls.temperature_url = '{}?'.format(cls.url, cls.params)
@@ -39,7 +34,8 @@ class TemperatureTest(APITestCase):
         """
         client = APIClient()
         response = self.client.get(self.temperature_url, self.temperature_list, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+        print(response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response)
         self.assertEqual(len(response.data), 1)
 
     # integration tests for edge cases:
@@ -69,4 +65,4 @@ class TemperatureTest(APITestCase):
     #     )
     #     self.assertEqual(
     #         response.data['end'], ['This field is required.']
-    #     )
+    #       )
